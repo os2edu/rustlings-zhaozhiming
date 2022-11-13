420f02fd8d67dd5e9cf0bda7de63babcfa7d3ed7
zhaozhiming <zhaozhiming003@gmail.com>
2022-11-13 14:47:50 +0000

    conversions done

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,25 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let vec: Vec<_> = s.split(',').collect();
+        if vec.len() != 2 {
+            Person::default()
+        } else {
+            let name = vec[0].to_string();
+            if name.len() == 0 {
+                return Person::default();
+            }
+            let age =  vec[1].parse();
+            match age {
+                Ok(x) => Person { 
+                    name, 
+                    age: x
+                },
+                Err(e) => Person::default(),
+            }
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -28,8 +28,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -46,6 +44,24 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty); 
+        }
+
+        let vec: Vec<_> = s.split(',').collect();
+        if vec.len() != 2 {
+            Err(ParsePersonError::BadLen)
+        } else {
+            let name = vec[0].to_string();
+            if name.len() == 0 {
+                return Err(ParsePersonError::NoName);
+            }
+            let age = vec[1].parse();
+            match age {
+                Ok(age) => Ok(Person { name, age }),
+                Err(e) => Err(ParsePersonError::ParseInt(e))
+            }
+        }
     }
 }
 
